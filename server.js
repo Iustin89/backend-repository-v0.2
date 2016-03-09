db940586928c55e3b523f09e60d0429f32e7197c
justin <damii_justin@yahoo.com>
2016-03-09 21:48:01 +0200

    Corectat!

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ var bodyParser = require('body-parser');
 var Product = require('./models/product');
 
 // our array
-app.use(bodyParser());
-mongoose.connect('mongodb://localhost:27017/productBD'); // nu reusesc sa ma conectez la baza de date!!!!
-
+mongoose.connect('mongodb://localhost:27017/productDB'); 
 
 
 // configure app to use bodyParser()
